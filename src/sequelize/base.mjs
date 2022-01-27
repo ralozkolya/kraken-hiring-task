6e412f22928687a0786f3f094dd7820b558726fb
Nikoloz Razmadze <raloz1@gmail.com>
2022-01-27 22:57:10 +0400

    Use SQLite instead of MySQL

--- src/sequelize/base.mjs
+++ src/sequelize/base.mjs
@@ -1,21 +1,7 @@
 import { Sequelize } from 'sequelize';
-import dotenv from 'dotenv';
-
-dotenv.config({ path: '../.env.local' });
-
-const {
-  MYSQL_USER,
-  MYSQL_PASSWORD,
-  MYSQL_DATABASE,
-  MYSQL_HOST,
-} = process.env;
 
 const sequelize = new Sequelize({
-  dialect: 'mysql',
-  host: MYSQL_HOST,
-  database: MYSQL_DATABASE,
-  username: MYSQL_USER,
-  password: MYSQL_PASSWORD,
+  dialect: 'sqlite',
   logging: false,
 });
 
